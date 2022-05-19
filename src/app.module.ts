ecb7ca14acb136e8f6ebf1f1fb9efc35e77e0521
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-05-18 23:10:02 -0300

    feat: add exception

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
 import { TypeOrmConfigModule } from './infrastructure/config/typeorm/typeorm.module';
 import { LoggerModule } from './infrastructure/logger/logger.module';
+import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
 
 @Module({
-  imports: [EnvironmentConfigModule, TypeOrmConfigModule, LoggerModule],
+  imports: [EnvironmentConfigModule, TypeOrmConfigModule, LoggerModule, ExceptionsModule],
   controllers: [],
   providers: [],
 })

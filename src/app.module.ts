1be868763b5e3071242353dd01dd934dc09d3da3
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-05-18 23:07:44 -0300

    feat: add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
 import { TypeOrmConfigModule } from './infrastructure/config/typeorm/typeorm.module';
+import { LoggerModule } from './infrastructure/logger/logger.module';
 
 @Module({
-  imports: [EnvironmentConfigModule, TypeOrmConfigModule],
+  imports: [EnvironmentConfigModule, TypeOrmConfigModule, LoggerModule],
   controllers: [],
   providers: [],
 })

823c826b6a9e871c1d8df68592e000ccf2b569f9
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-05-18 23:39:27 -0300

    feat: add a filter to catch  and format errors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './infrastructure/common/filter/exception.filter';
+import { LoggerService } from './infrastructure/logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Filter
+  app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+
   await app.listen(3000);
 }
 bootstrap();

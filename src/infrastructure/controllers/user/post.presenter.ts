4bb42c3bd897d18588ae92980adb1e8f0fd2d82a
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-05-21 16:52:28 -0300

    feat: add post presenter

---
+++ src/infrastructure/controllers/user/post.presenter.ts
@@ -0,0 +1,19 @@
+import { PostModel } from 'src/domain/model/post';
+
+export class PostPresenter {
+  id: string;
+  user_id: string;
+  content: string;
+  post_id_from?: string;
+  type: string;
+  created_at: Date;
+
+  constructor(post: PostModel) {
+    this.id = post.id;
+    this.user_id = post.user_id;
+    this.content = post.content;
+    this.post_id_from = post.post_id_from;
+    this.type = post.type;
+    this.created_at = post.created_at;
+  }
+}
